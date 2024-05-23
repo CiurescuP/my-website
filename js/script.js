942e4a3747dddfee6e504af13340b24cc5c458d9
Patricia Ciurescu <102490292+CiurescuP@users.noreply.github.com>
2024-05-23 15:15:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,96 @@
+/* HAMBURGER MENU */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// WHEN ON SCROLL DOWN, NAVBAR DISAPPEARS, WHEN SCROLLING BACK UP, NAVBAR COMES BACK OUT.
+var lastScrollTop;
+
+navbar = document.getElementById('navbar');
+window.addEventListener('scroll',function(){
+
+var scrollTop = document.documentElement.scrollTop;
+  if(scrollTop > lastScrollTop){
+  navbar.style.top='-80px';
+  }
+  else{
+  navbar.style.top='0';
+  }
+  lastScrollTop = scrollTop;
+});
+
+
+// CAROUSEL SLIDESHOW 
+var slideIndex = 1;
+var myTimer;
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+  
+    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+})
+
+// Next/previous controls
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  }
+}
+
+// Thumbnail image controls
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+pause = () => {
+  clearInterval(myTimer);
+}
+
+resume = () =>{
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+}
